1045cbfb5609043b7b0d923bd142688e0645d01c
Akhtar Jameel <akhtarjameel@gmail.com>
2014-11-18 00:00:56 -0800

    AWS modification v1.2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
-  config.active_record.schema_format = :ruby
 end
