45d5e0dedd951991a42ee36e79748cc3854c1cf4
Akhtar Jameel <akhtarjameel@gmail.com>
2015-08-02 23:46:41 -0700

    updates reports generation

--- scripts/GenerateMemoLinkCompletesReport.rb
+++ scripts/GenerateMemoLinkCompletesReport.rb
@@ -3,11 +3,12 @@ require 'csv'
 CSV.open('MemoLinkcompletes', 'w') do |csv|
 #  csv << "Titles"
   User.where("created_at > ?", (Time.now.midnight - 7.day)).each do |m|
-    puts "user x"
     print "created_at", m.created_at
+    print "m.SurveysCompleted: ", m.SurveysCompleted
+    print "m.SurveysCompleted.length: ", m.SurveysCompleted.length
     puts
     if m.SurveysCompleted.length > 0 then
-      if m.SurveysCompleted.flatten(2).at(1).include?('MemoLink') == true then
+      if m.SurveysCompleted.flatten(2).include?("MemoLink") == true then
         csv << m.created_at
         csv << m.SurveysCompleted.to_a.flatten
         puts "added a new row"
--- scripts/GenerateRFGCompletesReport.rb
+++ scripts/GenerateRFGCompletesReport.rb
@@ -3,9 +3,12 @@ require 'csv'
 CSV.open('RFGcompletes', 'w') do |csv|
 #  csv << "Titles"
   User.where("created_at > ?", (Time.now.midnight - 31.day)).each do |m|
-    puts "user x"
+    print "created_at", m.created_at
+    print "m.SurveysCompleted: ", m.SurveysCompleted
+    print "m.SurveysCompleted.length: ", m.SurveysCompleted.length
+    puts
     if m.SurveysCompleted.length > 0 then
-      if m.SurveysCompleted.flatten(2).at(1).include?('RFG') == true then
+      if m.SurveysCompleted.flatten(2).include?("RFG") == true then
         csv << m.created_at
         csv << m.SurveysCompleted.to_a.flatten
         puts "added a new row"
