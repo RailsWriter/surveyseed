eee0528d83e8059730b9801c9f4fe4f03c555f90
Akhtar Jameel <akhtarjameel@gmail.com>
2016-11-26 00:29:39 -0800

    Updated for draft_survey v6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   add_flash_types :error, :alert
+
+  before_filter :set_access
+
+def set_access
+  response.headers["Access-Control-Allow-Origin"] = "*"
+end
+
 end
