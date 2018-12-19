a9b42c5f2555a0d8b0ce35ad65a486c116cfc606
Akhtar Jameel <akhtarjameel@gmail.com>
2018-12-18 22:21:03 -0800

    Removed csv bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,15 +23,20 @@ class User < ActiveRecord::Base
 					if c.SurveysCompleted.flatten(2).include?("TESTSURVEY") then
 						print "******** Skip this record because it is a TESTSURVEY **********"
 					else
-						(0..c.SurveysCompleted.count-1).each do |i|
-							print "**********DateCompleted**************************", c.SurveysCompleted.flatten(2).at(-7-7*i).to_date
-							puts
-							if (c.SurveysCompleted.flatten(2).at(-7-7*i).to_date.mon >= Date.today.beginning_of_month.mon) && (c.SurveysCompleted.flatten(2).at(-7-7*i).to_date.mon <= Date.today.end_of_month.mon) then
-							# if (c.SurveysCompleted.flatten(2).at(-7-7*i).to_date.mon >= Date.today.last_month.beginning_of_month.mon) && (c.SurveysCompleted.flatten(2).at(-7-7*i).to_date.mon <= Date.today.last_month.end_of_month.mon) then
-								print "**********MonthCompleted**************************", c.SurveysCompleted.flatten(2).at(-7-7*i).to_date.mon
+						@SurveysCompletedArray = c.SurveysCompleted.flatten(2)
+						if (@SurveysCompletedArray[0].is_a?String) then
+          					# ignore - it is older storage format
+        				else
+							(0..c.SurveysCompleted.count-1).each do |i|
+								print "**********DateCompleted**************************", c.SurveysCompleted.flatten(2).at(-7-7*i).to_date
 								puts
-								csv << [c.SurveysCompleted.flatten(2).at(-7-7*i), "\"#{c.SurveysCompleted.flatten(2).at(-2-7*i)}\""]
-							else
+								if (c.SurveysCompleted.flatten(2).at(-7-7*i).to_date.mon >= Date.today.beginning_of_month.mon) && (c.SurveysCompleted.flatten(2).at(-7-7*i).to_date.mon <= Date.today.end_of_month.mon) then
+								# if (c.SurveysCompleted.flatten(2).at(-7-7*i).to_date.mon >= Date.today.last_month.beginning_of_month.mon) && (c.SurveysCompleted.flatten(2).at(-7-7*i).to_date.mon <= Date.today.last_month.end_of_month.mon) then
+									print "**********MonthCompleted**************************", c.SurveysCompleted.flatten(2).at(-7-7*i).to_date.mon
+									puts
+									csv << [c.SurveysCompleted.flatten(2).at(-7-7*i), "\"#{c.SurveysCompleted.flatten(2).at(-2-7*i)}\""]
+								else
+								end
 							end
 						end
 					end
@@ -56,15 +61,20 @@ class User < ActiveRecord::Base
 					if c.SurveysCompleted.flatten(2).include?("TESTSURVEY") then
 						print "******** Skip this record because it is a TESTSURVEY **********"
 					else
-						(0..c.SurveysCompleted.count-1).each do |i|
-							print "**********DateCompleted**************************", c.SurveysCompleted.flatten(2).at(-7-7*i).to_date
-							puts
-							# if (c.SurveysCompleted.flatten(2).at(-7-7*i).to_date.mon >= Date.today.beginning_of_month.mon) && (c.SurveysCompleted.flatten(2).at(-7-7*i).to_date.mon <= Date.today.end_of_month.mon) then
-							if (c.SurveysCompleted.flatten(2).at(-7-7*i).to_date.mon >= Date.today.last_month.beginning_of_month.mon) && (c.SurveysCompleted.flatten(2).at(-7-7*i).to_date.mon <= Date.today.last_month.end_of_month.mon) then
-								print "**********MonthCompleted**************************", c.SurveysCompleted.flatten(2).at(-7-7*i).to_date.mon
+						@SurveysCompletedArray = c.SurveysCompleted.flatten(2)
+						if (@SurveysCompletedArray[0].is_a?String) then
+          					# ignore - it is older storage format
+        				else
+							(0..c.SurveysCompleted.count-1).each do |i|
+								print "**********DateCompleted**************************", c.SurveysCompleted.flatten(2).at(-7-7*i).to_date
 								puts
-								csv << [c.SurveysCompleted.flatten(2).at(-7-7*i), "\"#{c.SurveysCompleted.flatten(2).at(-2-7*i)}\""]
-							else
+								# if (c.SurveysCompleted.flatten(2).at(-7-7*i).to_date.mon >= Date.today.beginning_of_month.mon) && (c.SurveysCompleted.flatten(2).at(-7-7*i).to_date.mon <= Date.today.end_of_month.mon) then
+								if (c.SurveysCompleted.flatten(2).at(-7-7*i).to_date.mon >= Date.today.last_month.beginning_of_month.mon) && (c.SurveysCompleted.flatten(2).at(-7-7*i).to_date.mon <= Date.today.last_month.end_of_month.mon) then
+									print "**********MonthCompleted**************************", c.SurveysCompleted.flatten(2).at(-7-7*i).to_date.mon
+									puts
+									csv << [c.SurveysCompleted.flatten(2).at(-7-7*i), "\"#{c.SurveysCompleted.flatten(2).at(-2-7*i)}\""]
+								else
+								end
 							end
 						end
 					end
